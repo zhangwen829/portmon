5bde0a5d380992f9c84f284cdeffe0c4845bac1d
zhangwen829 <zhangwenfd@gmail.com>
2018-06-14 18:16:04 -0400

    make backend runnable.

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 const passport = require('passport');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const db = require('./db');
-const sessionStore = new SequelizeStore({db});
+const sessionStore = new SequelizeStore({ db });
 const PORT = process.env.PORT || 8080;
 const app = express();
 const socketio = require('socket.io');
@@ -26,73 +26,73 @@ if (process.env.NODE_ENV !== 'production') require('../secrets');
 // passport registration
 passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(
-    (id, done) =>
-        db.models.user.findById(id).then(user => done(null, user)).catch(done));
+  (id, done) =>
+    db.models.user.findById(id).then(user => done(null, user)).catch(done));
 
 const createApp =
-    () => {
-      // logging middleware
-      app.use(morgan('dev'));
+  () => {
+    // logging middleware
+    app.use(morgan('dev'));
 
-      // body parsing middleware
-      app.use(bodyParser.json());
-      app.use(bodyParser.urlencoded({extended: true}));
+    // body parsing middleware
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-      // compression middleware
-      app.use(compression());
+    // compression middleware
+    app.use(compression());
 
-      // session middleware with passport
-      app.use(session({
-        secret: process.env.SESSION_SECRET || 'my best friend is Cody',
-        store: sessionStore,
-        resave: false,
-        saveUninitialized: false
-      }));
-      app.use(passport.initialize());
-      app.use(passport.session());
+    // session middleware with passport
+    app.use(session({
+      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+      store: sessionStore,
+      resave: false,
+      saveUninitialized: false
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
 
-      // auth and api routes
-      app.use('/auth', require('./auth'));
-      app.use('/api', require('./api'));
+    // auth and api routes
+    // app.use('/auth', require('./auth'));
+    app.use('/api', require('./api'));
 
-      // static file-serving middleware
-      app.use(express.static(path.join(__dirname, '..', 'public')));
+    // static file-serving middleware
+    app.use(express.static(path.join(__dirname, '..', 'public')));
 
-      // any remaining requests with an extension (.js, .css, etc.) send 404
-      app.use((req, res, next) => {
-        if (path.extname(req.path).length) {
-          const err = new Error('Not found');
-          err.status = 404;
-          next(err);
-        } else {
-          next();
-        }
-      });
+    // any remaining requests with an extension (.js, .css, etc.) send 404
+    app.use((req, res, next) => {
+      if (path.extname(req.path).length) {
+        const err = new Error('Not found');
+        err.status = 404;
+        next(err);
+      } else {
+        next();
+      }
+    });
 
-      // sends index.html
-      app.use('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '..', 'public/index.html'));
-      });
+    // sends index.html
+    app.use('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+    });
 
-      // error handling endware
-      app.use((err, req, res, next) => {
-        console.error(err);
-        console.error(err.stack);
-        res.status(err.status || 500)
-            .send(err.message || 'Internal server error.');
-      })
-    }
+    // error handling endware
+    app.use((err, req, res, next) => {
+      console.error(err);
+      console.error(err.stack);
+      res.status(err.status || 500)
+        .send(err.message || 'Internal server error.');
+    })
+  }
 
 const startListening =
-    () => {
-      // start listening (and create a 'server' object representing our server)
-      const server =
-          app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
+  () => {
+    // start listening (and create a 'server' object representing our server)
+    const server =
+      app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
 
-      // set up our socket control center
-      const io = socketio(server);
-      require('./socket')(io);
-    }
+    // set up our socket control center
+    const io = socketio(server);
+    require('./socket')(io);
+  }
 
 const syncDb = () => db.sync();
 
