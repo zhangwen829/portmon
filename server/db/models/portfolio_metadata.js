d8d322c2ff829da615cf5c331d71ab7be1d53b97
Wen Zhang <zhangwenfd@gmail.com>
2018-06-14 00:11:49 -0400

    PortfolioMetadata CURD methods

--- server/db/models/portfolio_metadata.js
+++ server/db/models/portfolio_metadata.js
@@ -16,9 +16,31 @@ const PortfolioMetadata = db.define('portfolio_metadata', {
   }
 });
 
-
+// CURD - Read
+// Class Method
 PortfolioMetadata.getPortfolioMetadataByUserId = (userId) => {
   this.findAll({ where: { userId } });
+};
+
+// CURD - Create
+// Class Method
+PortfolioMetadata.createNewPortfolio = (portfolioName) => {
+  this.create({
+    name: portfolioName
+  });
+};
+
+// CURD - Update
+// Instance Method
+PortfolioMetadata.prototype.changePortfolioName = async (portfolioId, newName) => {
+  const portfolio = await this.findOne({ where: { portfolioId } });
+  portfolio.name = newName;
+};
+
+// CURD - Delete
+// Instance Method
+PortfolioMetadata.prototype.deletePortfolio = (portfolioId) => {
+  PortfolioMetadata.destroy({ where: { portfolioId } });
 };
 
 module.exports = PortfolioMetadata;
