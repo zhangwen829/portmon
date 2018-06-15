c3720648d416e85ad177bc711a682ef64fa5fbe3
zhangwen829 <zhangwenfd@gmail.com>
2018-06-15 12:31:27 -0400

    should use menu rather than drawer

--- client/app.js
+++ client/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Routes from './routes';
-import Drawer from '@material-ui/core/Drawer';
 import AccountBox from '@material-ui/icons/AccountBox';
 import InboxIcon from '@material-ui/icons/Inbox';
 import List from '@material-ui/core/List';
@@ -8,20 +7,13 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import { withStyles } from '@material-ui/core/styles';
 import { MonitorTable } from './components';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const styles = theme => ({
   root: {
     display: 'flex',
-    position: 'relative',
-    width: '100%',
   },
-  drawerPaper: {
-    position: 'relative',
-    flexGrow: 1,
-  },
-  content: {
-    flexGrow: 1,
-  }
 });
 
 class App extends React.Component {
@@ -29,18 +21,23 @@ class App extends React.Component {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <Drawer classes={{
-          paper: classes.drawerPaper,
-        }} anchor='left' variant='permanent' open={false}>
-          <List component='nav'>
+        <MenuList>
+          <MenuItem>
             <ListItem button>
               <ListItemIcon><AccountBox /></ListItemIcon>
             </ListItem>
+          </MenuItem>
+          <MenuItem>
             <ListItem button>
               <ListItemIcon><InboxIcon /></ListItemIcon>
             </ListItem>
-          </List>
-        </Drawer>
+          </MenuItem>
+          <MenuItem>
+            <ListItem button>
+              <ListItemIcon><InboxIcon /></ListItemIcon>
+            </ListItem>
+          </MenuItem>
+        </MenuList>
         <MonitorTable />
       </div>
     );
