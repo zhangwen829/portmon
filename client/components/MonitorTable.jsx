5b705a6ccaa84dda4847412c13e2952bc02a7480
Wen Zhang <zhangwenfd@gmail.com>
2018-06-16 19:40:22 -0400

    add trend mock component

--- client/components/MonitorTable.jsx
+++ client/components/MonitorTable.jsx
@@ -6,6 +6,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { withStyles } from '@material-ui/core/styles';
+import Trend from 'react-trend';
 import io from 'socket.io-client';
 
 const styles = theme => ({
@@ -13,11 +14,11 @@ const styles = theme => ({
     margin: theme.spacing.unit * 5
   },
   table: {
-    backgroundColor: '#01579B'
+    backgroundColor: '#9E9E9E'
   },
   cell: {
     fontSize: 15,
-    color: '#FAFAFA'
+    color: '#212121'
   }
 });
 
@@ -31,6 +32,7 @@ const MonitorTable = (props) => {
             <TableCell className={classes.cell}>Ticker</TableCell>
             <TableCell className={classes.cell} numeric>Position</TableCell>
             <TableCell className={classes.cell} numeric>Last Price</TableCell>
+            <TableCell className={classes.cell}>Trend</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -38,16 +40,19 @@ const MonitorTable = (props) => {
             <TableCell className={classes.cell}>GOOG</TableCell>
             <TableCell className={classes.cell} numeric>10</TableCell>
             <TableCell className={classes.cell} numeric>1001</TableCell>
+            <TableCell><Trend data={[0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]} /></TableCell>
           </TableRow>
           <TableRow>
             <TableCell className={classes.cell}>IBM</TableCell>
             <TableCell className={classes.cell} numeric>100</TableCell>
             <TableCell className={classes.cell} numeric>137</TableCell>
+            <TableCell><Trend data={[0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]} /></TableCell>
           </TableRow>
           <TableRow>
             <TableCell className={classes.cell}>AMZN</TableCell>
             <TableCell className={classes.cell} numeric>38</TableCell>
             <TableCell className={classes.cell} numeric>1907</TableCell>
+            <TableCell><Trend data={[0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]} /></TableCell>
           </TableRow>
         </TableBody>
       </Table>
