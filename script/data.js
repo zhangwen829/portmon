b119960673dfa94acc86f1b143a4adcd3ed107e7
Wen Zhang <zhangwenfd@gmail.com>
2018-06-17 13:57:15 -0400

    more data for seedfile

--- script/data.js
+++ script/data.js
@@ -1,22 +1,34 @@
 /* eslint-disable */
 
-const dummyUser =
-    [{email: 'wen@google.com', firstName: 'Wen', lastName: 'Zhang'}];
+const dummyUser = [
+  {email: 'wen@google.com', firstName: 'Wen', lastName: 'Zhang'},
+  {email: 'ling@google.com', firstName: 'Ling', lastName: 'Xu'}
+];
 
 const dummySecurity = [
-  {ticker: 'GOOG', type: 'EQUITY'}, {ticker: 'AMZN', type: 'EQUITY'},
-  {ticker: 'TWRT', type: 'EQUITY'}, {ticker: 'FB', type: 'EQUITY'}
+  {ticker: 'GOOG', type: 'EQUITY'},
+  {ticker: 'AMZN', type: 'EQUITY'},
+  {ticker: 'TWRT', type: 'EQUITY'},
+  {ticker: 'FB', type: 'EQUITY'},
+  {ticker: 'MSFT', type: 'EQUITY'},
+  {ticker: 'EBAY', type: 'EQUITY'},
 ];
 
 const dummyPortfolioData = [
   {position: 100, portfolioMetadatumId: 1, securityId: 1},
   {position: 200, portfolioMetadatumId: 1, securityId: 3},
   {position: 50, portfolioMetadatumId: 2, securityId: 1},
-  {position: 300, portfolioMetadatumId: 2, securityId: 4}
+  {position: 300, portfolioMetadatumId: 2, securityId: 4},
+  {position: 150, portfolioMetadatumId: 3, securityId: 5},
+  {position: 100, portfolioMetadatumId: 4, securityId: 2},
+  {position: 200, portfolioMetadatumId: 4, securityId: 6}
+
 ];
 
-const dummyPortfolioMetadata =
-    [{name: 'Portfolio #1', userId: 1}, {name: 'Portfolio #2', userId: 1}];
+const dummyPortfolioMetadata = [
+  {name: 'Portfolio #1', userId: 1}, {name: 'Portfolio #2', userId: 1},
+  {name: 'Portfolio #3', userId: 1}, {name: 'Portfolio #1', userId: 2}
+];
 
 
 module.exports = {
